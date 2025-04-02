0b5cdcf8214ea822c59b78991e8d4aa8b8688595
Arjun-steam <Arjun@steam-a.com>
2025-04-02 20:11:03 +0530

    Initial Setup

---
+++ src/app.ts
@@ -0,0 +1,20 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+
+dotenv.config(); 
+const app = express();
+const port = process.env.PORT;
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+app.get('/check', (req, res) => {
+  res.send('API is working');
+});
+
+app.listen(port, () => {
+  return console.log(`Express is listening at http://localhost:${port}`);
+});
