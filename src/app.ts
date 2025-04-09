b05f00b91c38e05b3fad151e5fa6e26bdcfae970
Arjun-steam <Arjun@steam-a.com>
2025-04-09 10:31:08 +0530

    autocomplete apis

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import placeRouter from "./routes/placeRouter"
+import axios from 'axios';
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 8000;
@@ -17,7 +18,37 @@ app.get('/check', (req, res) => {
 });
 
 app.use('/api', placeRouter);
+app.get('/api/place-autocomplete', async (req, res) => {
+  const { input } = req.query;
+  try {
+    const response = await axios.get('https://maps.googleapis.com/maps/api/place/autocomplete/json', {
+      params: {
+        input,
+        key: process.env.GOOGLE_API_KEY,
+        components: 'country:IN',
+      },
+    });
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).json({ message: 'Failed to fetch data from Google API' });
+  }
+});
+app.get('/api/place-details', async (req, res) => {
+  const { placeid } = req.query;
+  try {
+    const response = await axios.get('https://maps.googleapis.com/maps/api/place/details/json', {
+      params: {
+        placeid,
+        key: process.env.GOOGLE_API_KEY,
+      },
+    });
 
+    res.json(response.data);
+  } catch (error) {
+    console.error('Error fetching place details:', error);
+    res.status(500).send({ message: 'Failed to fetch place details' });
+  }
+});
 app.listen(port, () => {
   console.log(`Express is listening at http://localhost:${port}`);
 });
